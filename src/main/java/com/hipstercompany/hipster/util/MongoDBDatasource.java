3abc831fe1301bec1e6e9244d2cb670da0976e63
christianseong <39890852+christianseong@users.noreply.github.com>
2020-06-10 14:32:21 +0900

    MongoDBDatsource 수정본
    2020-06-10 2시 30

--- src/main/java/com/hipstercompany/hipster/util/MongoDBDatasource.java
+++ src/main/java/com/hipstercompany/hipster/util/MongoDBDatasource.java
@@ -1,28 +1,41 @@
 package com.hipstercompany.hipster.util;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoDatabase;
 
 public class MongoDBDatasource {
 		public static void main(String [] args) {
+			
+			//Connect to MongoDB
 			MongoClientURI uri = new MongoClientURI("mongodb+srv://christian:christian@cluster0-5cnsr.mongodb.net/shoppingmall_db?retryWrites=true&w=majority");
 			MongoClient mongoClient = new MongoClient(uri);
 			
+			//View Database List
+			List<String> databases = mongoClient.getDatabaseNames();
 			
-			
-			DB database = mongoClient.getDB("shoppingmall_db");
-			
-			List<String> databases = new ArrayList<String>();
-			for (String databaseName :mongoClient.listDatabaseNames()) {
-				databases.add(databaseName);
+			System.out.println("=======Database List=======");
+			int dbnum =1;
+			for(String dbName :databases) {
+				System.out.println(dbnum+"."+dbName);
+				dbnum++;
 			}
+			System.out.println();
+			//Connect Database and show Collection List ¿¹½Ã) shoppingmall_db 
+			DB database = mongoClient.getDB("shoppingmall_db");
+			Set<String> collections = database.getCollectionNames();
+			System.out.println("Databse: "+"shoppingmall_db");
+			int colNum=1;
+			for(String colName: collections) {
+				System.out.println("Collection Number "+colNum+" is "+colName);
+				colNum++;
+			}
+			
 			
 			DBCollection dbCollection = (DBCollection) database.getCollection("member");
 			
