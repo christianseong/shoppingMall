21a796ccbca6ec3aeb8982c16fd175422c8151ca
christianseong <39890852+christianseong@users.noreply.github.com>
2020-06-12 00:20:49 +0900

    Kakao Api service controller
    2020-06-12

--- src/main/java/com/hipstercompany/hipster/controller/HomeController.java
+++ src/main/java/com/hipstercompany/hipster/controller/HomeController.java
@@ -4,15 +4,21 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.hipstercompany.hipster.service.KakaoAPIService;
+
 @Controller
 public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+    private KakaoAPIService kakao;
 	
 	@RequestMapping(value="/",method=RequestMethod.GET)
 		public String home(Locale locale, Model model) {
@@ -21,8 +27,8 @@ public class HomeController {
 	
 	@RequestMapping(value="/login",method=RequestMethod.GET)
 	public String login(@RequestParam("code")String code, Model model) {
-		model.addAttribute("code", code);
-		System.out.println("code : "+code);
+		String access_Token = kakao.getAccessToken(code);
+        System.out.println("controller access_token : " + access_Token);
 		return "login";
 	}
 }
